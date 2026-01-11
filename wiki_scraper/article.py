0eb3a9e6470d0acad4d9baa982c4ffe5935a0e10
Mateusz <burza.mati@gmail.com>
2026-01-11 11:18:26 +0100

    small adjustments

--- wiki_scraper/article.py
+++ wiki_scraper/article.py
@@ -1,5 +1,7 @@
 # Module containing implementation of class `Article`.
-# This class is responsible for parsing scrapped content.
+# This class represents an article
+# and is responsible for parsing scrapped content.
+from bs4 import BeautifulSoup
 import pandas as pd
 
 
@@ -7,6 +9,7 @@ class Article:
     def __init__(self, html_content, phrase):
         self.html_content = html_content
         self.phrase = phrase
+        self._parsed_content = BeautifulSoup(self.html_content, "html.parser")
 
     def get_first_paragraph(self):
         pass
