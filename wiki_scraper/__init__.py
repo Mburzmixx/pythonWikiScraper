6fd7cb2468d97380b54fdd247add1b03b7470190
Mateusz Burza <mb469200@students.mimuw.edu.pl>
2026-01-24 19:05:20 +0100

    finish __init__

--- wiki_scraper/__init__.py
+++ wiki_scraper/__init__.py
@@ -1,2 +1,15 @@
 # Package `wiki_scraper` implemented by Mateusz Burza.
 # It can be used to perform certain scraping operations and analyze some data.
+from wiki_scraper.scraper import Scraper
+from wiki_scraper.article import Article
+from wiki_scraper.controller import Controller
+from wiki_scraper.exceptions import ArticleNotFound
+from wiki_scraper.cli import get_args
+
+__all__ = [
+    "Scraper",
+    "Article",
+    "Controller",
+    "ArticleNotFound",
+    "get_args",
+]
